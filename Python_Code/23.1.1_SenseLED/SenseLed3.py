a99db2c324ecd56fa3195a7300ad5ac854207ff7
JudaV <judavecht@gmail.com>
2024-10-01 11:14:38 +0200

    add SenseLed3

---
+++ Python_Code/23.1.1_SenseLED/SenseLed3.py
@@ -0,0 +1,34 @@
+import pigpio
+
+ledPin = 18      # define ledPin
+sensorPin = 17   # define sensorPin
+
+# connect to raspberry pi IO pins:  
+pi = pigpio.pi()
+
+def setup():
+    pi.set_mode(sensorPin, pigpio.INPUT)  
+    pi.set_pull_up_down(sensorPin, pigpio.PUD_DOWN)
+    pi.set_mode(ledPin, pigpio.OUTPUT) 
+   
+
+def loop():
+    while True:
+        if pi.read(sensorPin): # if sensor read is high
+            pi.write(ledPin, 1)
+        else:
+            pi.write(ledPin, 0)
+
+
+def destroy():
+    pi.stop()
+    print(f"\nbye\n")
+
+
+if __name__ == '__main__':     # Program entrance
+    print ('Program is starting...')
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:  # Press ctrl-c to end the program.
+        destroy()
