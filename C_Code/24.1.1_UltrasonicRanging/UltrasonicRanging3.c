a1bad324dabcfe8b217ce59756ef867f169a7dcd
JudaV <judavecht@gmail.com>
2024-10-03 10:22:29 +0200

    first commit this file

---
+++ C_Code/24.1.1_UltrasonicRanging/UltrasonicRanging3.c
@@ -0,0 +1,97 @@
+/* gcc -o UltrasonicRanging3 UltrasonicRanging3.c -lpigpio -lpthread */
+
+#include <pigpio.h>
+#include <stdio.h>
+#include <signal.h>
+
+int trigPin = 23; // define trigger pin and echo pin
+int echoPin = 24;
+
+uint32_t highTick = 0;
+uint32_t lowTick = 0;
+uint32_t pulseTime = 0;
+float distance;
+
+volatile int keepRunning = 1;
+void intHandler(int dummy);
+void setup(int trigPin, int echoPin);
+void trigger();
+void receiveCallBack(int gpio, int level, uint32_t tick);
+
+int main(int argc, char *argv[])
+{
+    if (gpioInitialise() < 0)
+    {
+        printf("Error: failed to initialize the GPIO interface\n");
+        return 1;
+    }
+    else
+    {
+        printf("GPIO interface initialized\n");
+    }
+    setup(trigPin, echoPin);
+    signal(SIGINT, intHandler);
+
+    while (keepRunning)
+    {
+        trigger();
+        gpioSetAlertFunc(echoPin, receiveCallBack);
+        gpioSetAlertFunc(echoPin, receiveCallBack);
+        gpioSleep(PI_TIME_RELATIVE, 1, 0);
+        highTick = 0;
+        lowTick = 0;
+    }
+
+    gpioTerminate();
+    printf("\nBye\n");
+    return 0;
+}
+
+void intHandler(int dummy)
+{
+    keepRunning = 0;
+}
+
+/* The sensor will output high on sensing and low if not.
+   So the sensorPin, needs to be pull down (set to) on not
+   sensing. */
+void setup(int trigPin, int echoPin)
+{
+    gpioSetMode(trigPin, PI_OUTPUT);
+    gpioSetMode(echoPin, PI_INPUT);
+    // gpioSetPullUpDown(echoPin, PI_PUD_DOWN);
+    gpioGlitchFilter(echoPin, 100);
+}
+
+void trigger()
+{
+    gpioWrite(trigPin, 0);
+    gpioSleep(PI_TIME_RELATIVE, 1, 500); // wait 1,5s
+    gpioWrite(trigPin, 1);
+    gpioDelay(10); // send 10us pulse
+    gpioWrite(trigPin, 0);
+}
+
+void receiveCallBack(int gpio, int level, uint32_t tick)
+{
+    if (level == 1) // rising edge
+    {
+        highTick = tick;
+    }
+    else
+    {
+        lowTick = tick; // level 0 means falling edge
+    }
+    // printf("highTick: %d, lowTick %d\n", highTick, lowTick);
+    if ((highTick > 0) && (lowTick > 0))
+    {
+        pulseTime = (lowTick - highTick); // lowTick is later, higher number
+
+        if (pulseTime < 10000) // filter missed ticks
+        {
+            printf("pulse_time: %d\n", pulseTime);
+            distance = (float)(17 * pulseTime / 1000);
+            printf("The distance is:  %.2f cm\n", distance);
+        }
+    }
+}
